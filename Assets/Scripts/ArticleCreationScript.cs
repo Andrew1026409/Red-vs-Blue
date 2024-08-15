beea41d258538a97d2e7a413e2675f502ee16ec9
msmith158 <1026618@student.sae.edu.au>
2024-08-15 21:41:53 +0800

    Prototype of game is complete

--- Assets/Scripts/ArticleCreationScript.cs
+++ Assets/Scripts/ArticleCreationScript.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ArticleCreationScript : MonoBehaviour
 {
-    [Header("Values > General")]
+    [Header("Values")]
+    [SerializeField] private float segment3Time;
+    [SerializeField] private float segment3CommentsDelay;
+    [SerializeField] private string levelToLoadAtEnd;
     private int week1Article;
     private int week2Article;
     private int week3Article;
     private int week4Article;
     private int currentWeek = 1;
+
+    [Header("Object References")]
+    [SerializeField] private TextMeshProUGUI weekCounter;
+    [SerializeField] private GameObject comment1;
+    [SerializeField] private GameObject comment2;
+    [SerializeField] private GameObject comment3;
+    [SerializeField] private GameObject segment3;
+    [SerializeField] private GameObject segment4;
 
     public void SelectArticle(int article)
     {
@@ -37,14 +50,36 @@ public class ArticleCreationScript : MonoBehaviour
     public void SubmitArticle()
     {
         currentWeek++;
+        weekCounter.text = "Week " + currentWeek;
 
+        StartCoroutine(Segment3Timer());
+    }
+
+    public void Ending()
+    {
         if (currentWeek == 5)
         {
-            Debug.Log("RESULTS:\nWeek 1: " + week1Article + ", Week 2: " + week2Article + ", Week 3: " + week3Article + ", Week 4: " + week4Article);
+            Debug.Log("RESULTS:\nWeek 1: Article " + week1Article + "\nWeek 2: Article " + week2Article + "\nWeek 3: Article " + week3Article + "\nWeek 4: Article " + week4Article);
+
+            SceneManager.LoadScene(levelToLoadAtEnd);
         }
-        else
-        {
-            Debug.Log("Progressed to week " + currentWeek);
-        }
+    }
+
+    private IEnumerator Segment3Timer()
+    {
+        StartCoroutine(CommentsAppear());
+        yield return new WaitForSeconds(segment3Time);
+        segment3.SetActive(false);
+        segment4.SetActive(true);
+    }
+
+    private IEnumerator CommentsAppear()
+    {
+        yield return new WaitForSeconds(segment3CommentsDelay);
+        comment1.SetActive(true);
+        yield return new WaitForSeconds(segment3CommentsDelay);
+        comment2.SetActive(true);
+        yield return new WaitForSeconds(segment3CommentsDelay);
+        comment3.SetActive(true);
     }
 }
--- Assets/Scripts/LevelLoading.cs
+++ Assets/Scripts/LevelLoading.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelLoadingTrigger : MonoBehaviour
+public class LevelLoading : MonoBehaviour
 {
     [SerializeField] private string levelToLoad;
 
     private void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void LoadLevel()
     {
         SceneManager.LoadScene(levelToLoad);
     }
