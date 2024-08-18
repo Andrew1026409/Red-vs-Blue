0d1a69cd8874bf87b14887fb491b67509efc6c10
msmith158 <1026618@student.sae.edu.au>
2024-08-18 20:51:01 +0800

    Segments 2 and 3 are ready to go

--- Assets/Scripts/ArticleCreationScript.cs
+++ Assets/Scripts/ArticleCreationScript.cs
@@ -7,44 +7,62 @@ using UnityEngine.UI;
 
 public class ArticleCreationScript : MonoBehaviour
 {
-    [Header("Values > Segment 1")]
+    [Header("===== Values > Segment 1 =====")]
     [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> weeklyArticle1Drafts = new List<string>();
     [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> weeklyArticle2Drafts = new List<string>();
     [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> weeklyArticle3Drafts = new List<string>();
     private int week1Article;
+    private int week2Article;
+    private int week3Article;
+    private int week4Article;
     private int currentWeek = 1;
 
-    [Header("Values > Segment 2")]
+    [Header("===== Values > Segment 2 =====")]
     [SerializeField] private TextMeshProUGUI segment2HeaderText;
     [SerializeField] private TextMeshProUGUI segment2BodyText;
     [SerializeField] private Image segment2Image;
-    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> submittingArticleHeadline = new List<string>();
-    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> submittingArticleBody = new List<string>();
-    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<Sprite> submittingArticleImage = new List<Sprite>();
-    private int week2Article;
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> submittingArticle1Body = new List<string>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<Sprite> submittingArticle1Image = new List<Sprite>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> submittingArticle2Body = new List<string>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<Sprite> submittingArticle2Image = new List<Sprite>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> submittingArticle3Body = new List<string>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<Sprite> submittingArticle3Image = new List<Sprite>();
 
-    [Header("Values > Segment 3")]
+    [Header("===== Values > Segment 3 =====")]
+    [SerializeField] private TextMeshProUGUI segment3HeaderText;
+    [SerializeField] private TextMeshProUGUI segment3BodyText;
+    [SerializeField] private TextMeshProUGUI comment1BodyText;
+    [SerializeField] private TextMeshProUGUI comment2BodyText;
+    [SerializeField] private TextMeshProUGUI comment3BodyText;
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> article1Comment1Body = new List<string>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> article1Comment2Body = new List<string>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> article1Comment3Body = new List<string>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> article2Comment1Body = new List<string>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> article2Comment2Body = new List<string>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> article2Comment3Body = new List<string>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> article3Comment1Body = new List<string>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> article3Comment2Body = new List<string>();
+    [Tooltip("Each subsequent entry into this list is counted as the next week, e.g. entry 2 is counted as week 2")] [SerializeField] private List<string> article3Comment3Body = new List<string>();
+    [SerializeField] private Image segment3Image;
     [SerializeField] private float segment3Time;
     [SerializeField] private float segment3CommentsDelay;
-    private int week3Article;
 
-    [Header("Values > Segment 4")]
+    [Header("===== Values > Segment 4 =====")]
     [SerializeField] private string levelToLoadAtEnd;
-    private int week4Article;
 
-    [Header("Object References > Segment 1")]
+    [Header("===== Object References > Segment 1 =====")]
     [SerializeField] private TextMeshProUGUI weekCounter;
     [SerializeField] private GameObject article1;
     [SerializeField] private GameObject article2;
     [SerializeField] private GameObject article3;
 
-    [Header("Object References > Segment 3")]
+    [Header("===== Object References > Segment 3 =====")]
     [SerializeField] private GameObject segment3;
     [SerializeField] private GameObject comment1;
     [SerializeField] private GameObject comment2;
     [SerializeField] private GameObject comment3;
 
-    [Header("Object References > Segment 4")]
+    [Header("===== Object References > Segment 4 =====")]
     [SerializeField] private GameObject segment4;
 
     private void OnEnable()
@@ -56,7 +74,7 @@ public class ArticleCreationScript : MonoBehaviour
     {
         switch (segment)
         {
-            case 0:
+            case 0: // FOR UPDATING THE DRAFT ARTICLES EACH WEEK
                 switch (currentWeek)
                 {
                     case 1:
@@ -81,12 +99,186 @@ public class ArticleCreationScript : MonoBehaviour
                         break;
                 }
                 break;
-            /*case 1:
+            case 1: // FOR UPDATING THE SEGMENT 2 SUBMITTING ARTICLE
                 switch (currentWeek)
                 {
-                    case
+                    case 1:
+                        switch (week1Article)
+                        {
+                            case 1:
+                                segment2HeaderText.text = weeklyArticle1Drafts[0];
+                                segment2BodyText.text = submittingArticle1Body[0];
+                                segment2Image.sprite = submittingArticle1Image[0];
+                                break;
+                            case 2:
+                                segment2HeaderText.text = weeklyArticle2Drafts[0];
+                                segment2BodyText.text = submittingArticle2Body[0];
+                                segment2Image.sprite = submittingArticle2Image[0];
+                                break;
+                            case 3:
+                                segment2HeaderText.text = weeklyArticle3Drafts[0];
+                                segment2BodyText.text = submittingArticle3Body[0];
+                                segment2Image.sprite = submittingArticle3Image[0];
+                                break;
+                        }
+                        break;
+                    case 2:
+                        switch (week2Article)
+                        {
+                            case 1:
+                                segment2HeaderText.text = weeklyArticle1Drafts[1];
+                                segment2BodyText.text = submittingArticle1Body[1];
+                                segment2Image.sprite = submittingArticle1Image[1];
+                                break;
+                            case 2:
+                                segment2HeaderText.text = weeklyArticle2Drafts[1];
+                                segment2BodyText.text = submittingArticle2Body[1];
+                                segment2Image.sprite = submittingArticle2Image[1];
+                                break;
+                            case 3:
+                                segment2HeaderText.text = weeklyArticle3Drafts[1];
+                                segment2BodyText.text = submittingArticle3Body[1];
+                                segment2Image.sprite = submittingArticle3Image[1];
+                                break;
+                        }
+                        break;
+                    case 3:
+                        switch (week3Article)
+                        {
+                            case 1:
+                                segment2HeaderText.text = weeklyArticle1Drafts[2];
+                                segment2BodyText.text = submittingArticle1Body[2];
+                                segment2Image.sprite = submittingArticle1Image[2];
+                                break;
+                            case 2:
+                                segment2HeaderText.text = weeklyArticle2Drafts[2];
+                                segment2BodyText.text = submittingArticle2Body[2];
+                                segment2Image.sprite = submittingArticle2Image[2];
+                                break;
+                            case 3:
+                                segment2HeaderText.text = weeklyArticle3Drafts[2];
+                                segment2BodyText.text = submittingArticle3Body[2];
+                                segment2Image.sprite = submittingArticle3Image[2];
+                                break;
+                        }
+                        break;
+                    case 4:
+                        switch (week4Article)
+                        {
+                            case 1:
+                                segment2HeaderText.text = weeklyArticle1Drafts[3];
+                                segment2BodyText.text = submittingArticle1Body[3];
+                                segment2Image.sprite = submittingArticle1Image[3];
+                                break;
+                            case 2:
+                                segment2HeaderText.text = weeklyArticle2Drafts[3];
+                                segment2BodyText.text = submittingArticle2Body[3];
+                                segment2Image.sprite = submittingArticle2Image[3];
+                                break;
+                            case 3:
+                                segment2HeaderText.text = weeklyArticle3Drafts[3];
+                                segment2BodyText.text = submittingArticle3Body[3];
+                                segment2Image.sprite = submittingArticle3Image[3];
+                                break;
+                        }
+                        break;
                 }
-            */
+                break;
+            case 2: // FOR CHANGING WHAT THE POSTED ARTICLE AND COMMENTS SAY
+                segment3HeaderText.text = segment2HeaderText.text;
+                segment3BodyText.text = segment2BodyText.text;
+                segment3Image.sprite = segment2Image.sprite;
+                Debug.Log(currentWeek);
+                Debug.Log(week1Article);
+                switch (currentWeek)
+                {
+                    case 1:
+                        switch (week1Article)
+                        {
+                            case 1:
+                                comment1BodyText.text = article1Comment1Body[0];
+                                comment2BodyText.text = article1Comment2Body[0];
+                                comment3BodyText.text = article1Comment3Body[0];
+                                break;
+                            case 2:
+                                comment1BodyText.text = article2Comment1Body[0];
+                                comment2BodyText.text = article2Comment2Body[0];
+                                comment3BodyText.text = article2Comment3Body[0];
+                                break;
+                            case 3:
+                                comment1BodyText.text = article3Comment1Body[0];
+                                comment2BodyText.text = article3Comment2Body[0];
+                                comment3BodyText.text = article3Comment3Body[0];
+                                break;
+                        }
+
+                        break;
+                    case 2:
+                        switch (week2Article)
+                        {
+                            case 1:
+                                comment1BodyText.text = article1Comment1Body[1];
+                                comment2BodyText.text = article1Comment2Body[1];
+                                comment3BodyText.text = article1Comment3Body[1];
+                                break;
+                            case 2:
+                                comment1BodyText.text = article2Comment1Body[1];
+                                comment2BodyText.text = article2Comment2Body[1];
+                                comment3BodyText.text = article2Comment3Body[1];
+                                break;
+                            case 3:
+                                comment1BodyText.text = article3Comment1Body[1];
+                                comment2BodyText.text = article3Comment2Body[1];
+                                comment3BodyText.text = article3Comment3Body[1];
+                                break;
+                        }
+
+                        break;
+                    case 3:
+                        switch (week3Article)
+                        {
+                            case 1:
+                                comment1BodyText.text = article1Comment1Body[2];
+                                comment2BodyText.text = article1Comment2Body[2];
+                                comment3BodyText.text = article1Comment3Body[2];
+                                break;
+                            case 2:
+                                comment1BodyText.text = article2Comment1Body[2];
+                                comment2BodyText.text = article2Comment2Body[2];
+                                comment3BodyText.text = article2Comment3Body[2];
+                                break;
+                            case 3:
+                                comment1BodyText.text = article3Comment1Body[2];
+                                comment2BodyText.text = article3Comment2Body[2];
+                                comment3BodyText.text = article3Comment3Body[2];
+                                break;
+                        }
+
+                        break;
+                    case 4:
+                        switch (week4Article)
+                        {
+                            case 1:
+                                comment1BodyText.text = article1Comment1Body[3];
+                                comment2BodyText.text = article1Comment2Body[3];
+                                comment3BodyText.text = article1Comment3Body[3];
+                                break;
+                            case 2:
+                                comment1BodyText.text = article2Comment1Body[3];
+                                comment2BodyText.text = article2Comment2Body[3];
+                                comment3BodyText.text = article2Comment3Body[3];
+                                break;
+                            case 3:
+                                comment1BodyText.text = article3Comment1Body[3];
+                                comment2BodyText.text = article3Comment2Body[3];
+                                comment3BodyText.text = article3Comment3Body[3];
+                                break;
+                        }
+                        break;
+                }
+                break;
+            case 3: // FOR CHANGING THE BOSS' EMAIL SUBJECT LINE AND BODY 
+                break;
         }
     }
 
@@ -115,14 +307,14 @@ public class ArticleCreationScript : MonoBehaviour
 
     public void SubmitArticle()
     {
-        currentWeek++;
-        weekCounter.text = "Week " + currentWeek;
-
         StartCoroutine(Segment3Timer());
     }
 
     public void Ending()
     {
+        currentWeek++;
+        weekCounter.text = "Week " + currentWeek;
+        
         if (currentWeek == 5)
         {
             Debug.Log("RESULTS:\nWeek 1: Article " + week1Article + "\nWeek 2: Article " + week2Article + "\nWeek 3: Article " + week3Article + "\nWeek 4: Article " + week4Article);
